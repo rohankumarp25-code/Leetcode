7a3a172d571b4b8c745418671bf26bab79aa87cf
Rohan Kumar p <rohankumarp25@gmail.com>
2026-06-28 15:24:01 +0530

    Richest Customer Wealth sol

---
+++ richestwealth.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int rows = accounts.size();
+        int cols = accounts[0].size();
+        int maxwealth=0;
+        int currentwealth=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                currentwealth=accounts[i][j]+currentwealth;
+            }
+            if(currentwealth>maxwealth){
+                maxwealth=currentwealth;
+            }
+            currentwealth=0;
+        }
+        return maxwealth;
+    }
+};
