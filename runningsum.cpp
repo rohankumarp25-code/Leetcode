e1b9771b0ac454d3180c7d78577fbbf8d4a9fd5e
Rohan Kumar p <rohankumarp25@gmail.com>
2026-06-28 14:53:20 +0530

    Running Sum of 1D Array sol

---
+++ runningsum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> run(nums.size(), 0);
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0)
+                run[i] = nums[i];
+            if (i > 0)
+                run[i] = run[i - 1] + nums[i];
+        }
+        return run;
+    }
+};
