69a6be910b0549992ee4663dc59a9c707324c9a0
Rohan Kumar p <rohankumarp25@gmail.com>
2026-07-01 19:20:53 +0530

    Contains Duplicate better  solution

--- ContainsDuplicate.cpp
+++ ContainsDuplicate.cpp
@@ -2,11 +2,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]]==2)
+            return true;
         }
         return false;
     }
